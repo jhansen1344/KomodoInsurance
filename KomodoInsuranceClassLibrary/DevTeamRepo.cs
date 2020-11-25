2c89f1f760710c571928cfeaee3130abd5913483
Jeremy Hansen <jhansen1344@gmail.com>
2020-11-24 21:12:38 -0500

    repo crud added

--- KomodoInsuranceClassLibrary/DevTeamRepo.cs
+++ KomodoInsuranceClassLibrary/DevTeamRepo.cs
@@ -12,8 +12,44 @@ namespace KomodoInsuranceClassLibrary
         private List<DevTeam> _teamDirectory = new List<DevTeam>();
         private int _currentDevId = 0;
         //Create
+        public bool CreateTeam(string name, List<Developer> devsOnTeam)
+        {
+            int startingNumberOfTeams = _teamDirectory.Count;
+            DevTeam teamToAdd = new DevTeam()
+            {
+                Name = name,
+                Developers = devsOnTeam,
+                Id = _currentDevId
+            };
+            _currentDevId++;
+            _teamDirectory.Add(teamToAdd);
+            return _teamDirectory.Count > startingNumberOfTeams;
+
+        }
         //Return
+        public List<DevTeam> ReturnTeams()
+        {
+            return _teamDirectory;
+        }
         //Update
+
+        public bool UpdateTeam()
+        {
+
+        }
+
         //Delete
+        public bool DeleteTeam(int id)
+        {
+            foreach (var team in _teamDirectory)
+            {
+                if(id==team.Id)
+                {
+                    _teamDirectory.Remove(team);
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
