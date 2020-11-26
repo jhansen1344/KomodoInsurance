c8b2d37579b6c9d2f1aa2e1b3d2c13a5443f6cb8
Jeremy Hansen <jhansen1344@gmail.com>
2020-11-25 19:49:09 -0500

    createDeveloper method added and helper methods aded

--- KomodoInsuranceUI/ProgramUI.cs
+++ KomodoInsuranceUI/ProgramUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KomodoInsuranceClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace KomodoInsuranceUI
 {
     public class ProgramUI
     {
+        private DeveloperRepo _devRepo = new DeveloperRepo();
+        private DevTeamRepo _teamRepo = new DevTeamRepo();
         public void Run()
         {
             Menu();
@@ -41,35 +44,120 @@ namespace KomodoInsuranceUI
                 switch (input)
                 {
                     case "1":
+                        CreateDeveloper();
                         break;
                     case "2":
+                        ViewDevelopers();
                         break;
                     case "3":
+                        ViewDeveloperByName();
                         break;
                     case "4":
+                        UpdateDeveloper();
                         break;
                     case "5":
+                        DeleteDeveloper();
                         break;
                     case "6":
+                        CreateTeam();
                         break;
                     case "7":
+                        ViewTeams();
                         break;
                     case "8":
+                        ViewTeamByName();
                         break;
                     case "9":
+                        UpdateTeam();
                         break;
                     case "10":
+                        DeleteTeam();
                         break;
                     case "11":
+                        Console.WriteLine("GoodBye");
                         keepRunning = false;
                         break;
                     default:
                         Console.WriteLine("Please Enter a Valid Menu Option");
                         break;
                 }
+                Console.WriteLine("Please press any key to continue");
+                Console.ReadKey();
+                Console.Clear();
             }
 
 
         }
+
+        private void CreateDeveloper()
+        {
+            string name = GetAndValidateInput("Please enter the developer's full name");
+            bool hasPluralSight = GetAndValidateBool("Does the developer have access to PluralSight? (y/n)");
+            _devRepo.AddDeveloper(name, hasPluralSight);
+        }
+
+        private void ViewDevelopers()
+        {
+
+        }
+
+        private void ViewDeveloperByName()
+        {
+
+        }
+
+        private void UpdateDeveloper()
+        {
+
+        }
+
+        private void DeleteDeveloper()
+        {
+
+        }
+
+        private void CreateTeam()
+        {
+
+        }
+
+        private void ViewTeams()
+        {
+
+        }
+
+        private void ViewTeamByName()
+        {
+
+        }
+
+        private void UpdateTeam()
+        {
+
+        }
+
+        private void DeleteTeam()
+        {
+
+        }
+
+        private string GetAndValidateInput(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        private bool GetAndValidateBool(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine().ToLower();
+            if(input =="y")
+            {
+            return true;
+            }
+            return false;
+        }
+
     }
 }
