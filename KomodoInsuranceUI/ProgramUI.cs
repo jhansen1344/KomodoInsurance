106a18bb3320b21feca5cb1affbd48790950bf82
Jeremy Hansen <jhansen1344@gmail.com>
2020-11-25 19:21:31 -0500

    ui methods scaffolded

--- KomodoInsuranceUI/ProgramUI.cs
+++ KomodoInsuranceUI/ProgramUI.cs
@@ -10,6 +10,65 @@ namespace KomodoInsuranceUI
     {
         public void Run()
         {
+            Menu();
+        }
+
+        private void Menu()
+        {
+
+            bool keepRunning = true;
+
+            while (keepRunning)
+            {
+
+
+                string menu = "Select a menu option:\n" +
+                    "1. Create New Developer\n" +
+                    "2. View Developers\n" +
+                    "3. View Developer By Name\n" +
+                    "4. Update Developer\n" +
+                    "5. Delete Developer\n" +
+                    "6. Create New Team\n" +
+                    "7. View Teams\n" +
+                    "8. View Team By Name\n" +
+                    "9. Update Team\n" +
+                    "10. Delete Team\n" +
+                    "11. Exit\n";
+
+                Console.WriteLine(menu);
+
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    case "8":
+                        break;
+                    case "9":
+                        break;
+                    case "10":
+                        break;
+                    case "11":
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter a Valid Menu Option");
+                        break;
+                }
+            }
+
 
         }
     }
